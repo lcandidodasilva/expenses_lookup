216c443abb1c54fed70af3a172198fc05d5ae429
Leonel Candido da Silva <leonelcs@gmail.com>
2025-03-10 21:55:19 +0100

    fixing production with database

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  },
+  experimental: {
+    serverActions: true,
+  },
 }
 
 module.exports = nextConfig
